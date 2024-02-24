ecf69d611e8982ec2ddee02cf1c80d3b173db46c
Hridaya Subedi <hridayasagarsubedi@gmail.com>
2024-02-24 15:58:16 +0000

    Transforming Location and Dropoff Id column for FHV table

--- 04_analytics_engineering/taxi_rides_ny/models/staging/homework/stg_homework__external_fhv_tripdata.sql
+++ 04_analytics_engineering/taxi_rides_ny/models/staging/homework/stg_homework__external_fhv_tripdata.sql
@@ -1,31 +1,32 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("homework", "external_fhv_tripdata") }}),
 
-    select * from {{ source('homework', 'external_fhv_tripdata') }}
+    renamed as (
 
-),
+        select
+            sr_flag,
+            dispatching_base_num,
+            affiliated_base_number,
 
-renamed as (
+            -- pickup_datetime,
+            -- dropoff_datetime,
+            -- timestamps
+            cast(pickup_datetime as timestamp) as pickup_datetime,
+            cast(dropoff_datetime as timestamp) as dropoff_datetime,
+            
+            {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }}
+            as pu_locationid,
+            {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }}
+            as do_locationid,
+        -- cast(pulocationid as numeric) as pu_locationid,
+        -- cast(dolocationid as numeric) as do_locationid
+        from source
 
-    select
-        sr_flag,
-        dispatching_base_num,
-        affiliated_base_number,
-        pickup_datetime,
-        dropoff_datetime,
-        pulocationid,
-        dolocationid
+    )
 
-    from source
-
-)
-
-select * from renamed
+select *
+from renamed
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
+{% if var("is_test_run", default=true) %} limit 100 {% endif %}
